2079097af414fc6a7729b7c51e21ddb00ff97cb0
Alireza <alireza83safarii@gmail.com>
2023-07-31 17:31:02 +0330

    use costum hook for form validations

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,42 @@
 import React from "react";
-import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  emailValidator,
+  maxValidator,
+  minValidator,
+  requiredValidator,
+} from "../validators/rules";
+import Input from "../components/Form/Input";
+import { useForm } from "../hooks/useForm";
 
 export default function Login() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  const navigate = useNavigate();
+  const [formState, onInputHandler] = useForm(
+    {
+      email: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
 
-  const onSubmit = (data) => {
-    console.log("Submitted:", data);
+  const userLogin = (event) => {
+    event.preventDefault();
+    const { email, password } = formState.inputs;
+
+    let userInfo = {
+      email: email,
+      password: password,
+    };
   };
-
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <form
-        className="w-96 p-6 rounded-lg shadow-md bg-white-300"
-        onSubmit={handleSubmit(onSubmit)}
-      >
+    <div className="flex items-center justify-center my-12">
+      <form className="w-96 p-6 rounded-lg shadow-md bg-white-300">
         <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
         <div className="mb-4 mt-12">
           <label
@@ -27,23 +45,20 @@ export default function Login() {
           >
             Email
           </label>
-          <input
+          <Input
             type="email"
             id="email"
-            className={`p-2 block w-full rounded-md border shadow-sm outline-none ${
-              errors.email ? "border-red-500" : "border-gray-300"
-            }`}
-            {...register("email", {
-              required: "Email is required",
-              pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                message: "Invalid email address",
-              },
-            })}
+            element="input"
+            placeholder="Email"
+            className="p-2 block w-full rounded-md border shadow-sm outline-none"
+            validations={[
+              requiredValidator(),
+              minValidator(8),
+              maxValidator(26),
+              emailValidator(),
+            ]}
+            onInputHandler={onInputHandler}
           />
-          {errors.email && (
-            <p className="text-red-700 text-xs mt-1">{errors.email.message}</p>
-          )}
         </div>
         <div className="mb-4 mt-6">
           <label
@@ -52,29 +67,25 @@ export default function Login() {
           >
             Password
           </label>
-          <input
+          <Input
             type="password"
             id="password"
-            className={`p-2 block w-full rounded-md border shadow-sm outline-none ${
-              errors.password ? "border-red-500" : "border-gray-300"
-            }`}
-            {...register("password", {
-              required: "Password is required",
-              minLength: {
-                value: 6,
-                message: "Password must have at least 6 characters",
-              },
-            })}
+            element="input"
+            placeholder="password"
+            className="p-2 block w-full rounded-md border shadow-sm outline-none"
+            validations={[
+              requiredValidator(),
+              minValidator(8),
+              maxValidator(26),
+            ]}
+            onInputHandler={onInputHandler}
           />
-          {errors.password && (
-            <p className="text-red-700 text-xs mt-1">
-              {errors.password.message}
-            </p>
-          )}
         </div>
         <button
           type="submit"
           className="w-full mt-8 py-2 px-4 bg-blue-600 hover:bg-blue-700 duration-300 text-white-100 rounded-lg focus:outline-none"
+          disabled={!formState.isFormValid}
+          onClick={userLogin}
         >
           Login
         </button>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,42 +1,73 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "../hooks/useForm";
+import {
+  emailValidator,
+  maxValidator,
+  minValidator,
+  requiredValidator,
+} from "../validators/rules";
+import Input from "../components/Form/Input";
 
 export default function Register() {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm();
+  const navigate = useNavigate();
+  const [sameValue, setSameValue] = useState(false);
+  const [formState, onInputHandler] = useForm(
+    {
+      username: {
+        value: "",
+        isValid: false,
+      },
+      email: {
+        value: "",
+        isValid: false,
+      },
+      password: {
+        value: "",
+        isValid: false,
+      },
+      confirmPassword: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
 
-  const sendUserData = async (data) => {
-    const { password, confirmPassword } = data;
 
-    // Check if the passwords match
-    if (password !== confirmPassword) {
-      alert("Passwords do not match!");
+  useEffect(() => {
+    const { password, confirmPassword } = formState.inputs;
+    setSameValue(password.value === confirmPassword.value);
+  }, [formState.inputs.password, formState.inputs.confirmPassword]);
+
+  const sendUserData = (event) => {
+    event.preventDefault();
+    const { password, confirmPassword } = formState.inputs;
+
+    if (password.value !== confirmPassword.value) {
+      setSameValue(false);
       return;
     }
 
+    let newUserInfo = {
+      username: formState.inputs.username.value,
+      email: formState.inputs.email.value,
+      password: formState.inputs.password.value,
+    };
     fetch("http://localhost:9000/user", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(data),
+      body: JSON.stringify(newUserInfo),
     })
       .then((res) => res.json())
       .then((res) => console.log(res));
-
-    reset();
   };
+
   return (
-    <div className="flex items-center justify-center min-h-screen my-12">
-      <form
-        className="w-96 p-6 rounded-lg shadow-md bg-white-300"
-        onSubmit={handleSubmit(sendUserData)}
-      >
+    <div className="flex items-center justify-center my-12">
+      <form className="w-96 p-6 rounded-lg shadow-md bg-white-300">
         <h2 className="text-2xl font-bold mb-6 text-center">Register</h2>
         <div className="mb-4 mt-6">
           <label
@@ -45,20 +76,21 @@ export default function Register() {
           >
             Username
           </label>
-          <input
+          <Input
             type="text"
             id="username"
-            className={`p-2 block w-full rounded-md border shadow-sm outline-none ${
-              errors.username ? "border-red-700" : "border-gray-300"
-            }`}
-            {...register("username", { required: "Username is required" })}
+            element="input"
+            placeholder="Username"
+            className="p-2 block w-full rounded-md border shadow-sm outline-none"
+            validations={[
+              requiredValidator(),
+              minValidator(6),
+              maxValidator(20),
+            ]}
+            onInputHandler={onInputHandler}
           />
-          {errors.username && (
-            <p className="text-red-700 text-xs mt-1">
-              {errors.username.message}
-            </p>
-          )}
         </div>
+
         <div className="mb-4 mt-6">
           <label
             htmlFor="email"
@@ -66,24 +98,22 @@ export default function Register() {
           >
             Email
           </label>
-          <input
+          <Input
             type="email"
             id="email"
-            className={`p-2 block w-full rounded-md border shadow-sm outline-none ${
-              errors.email ? "border-red-700" : "border-gray-300"
-            }`}
-            {...register("email", {
-              required: "Email is required",
-              pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                message: "Invalid email address",
-              },
-            })}
+            element="input"
+            placeholder="Email"
+            className="p-2 block w-full rounded-md border shadow-sm outline-none"
+            validations={[
+              requiredValidator(),
+              minValidator(8),
+              maxValidator(26),
+              emailValidator(),
+            ]}
+            onInputHandler={onInputHandler}
           />
-          {errors.email && (
-            <p className="text-red-700 text-xs mt-1">{errors.email.message}</p>
-          )}
         </div>
+
         <div className="mb-4 mt-6">
           <label
             htmlFor="password"
@@ -91,27 +121,21 @@ export default function Register() {
           >
             Password
           </label>
-          <input
+          <Input
             type="password"
             id="password"
-            className={`p-2 block w-full rounded-md border shadow-sm outline-none ${
-              errors.password ? "border-red-700" : "border-gray-300"
-            }`}
-            {...register("password", {
-              required: "Password is required",
-              minLength: {
-                value: 6,
-                message: "Password must have at least 6 characters",
-              },
-            })}
+            element="passwordRegister"
+            placeholder="password"
+            className="p-2 block w-full rounded-md border shadow-sm outline-none"
+            validations={[
+              requiredValidator(),
+              minValidator(8),
+              maxValidator(26),
+            ]}
+            onInputHandler={onInputHandler}
+            sameValue={sameValue}
           />
-          {errors.password && (
-            <p className="text-red-700 text-xs mt-1">
-              {errors.password.message}
-            </p>
-          )}
         </div>
-
         <div className="mb-4 mt-6">
           <label
             htmlFor="confirmPassword"
@@ -119,28 +143,25 @@ export default function Register() {
           >
             Confirm Password
           </label>
-          <input
+          <Input
             type="password"
             id="confirmPassword"
-            className={`p-2 block w-full rounded-md border shadow-sm outline-none ${
-              errors.password ? "border-red-700" : "border-gray-300"
-            }`}
-            {...register("confirmPassword", {
-              required: "Password is required",
-              minLength: {
-                value: 6,
-                message: "Password must have at least 6 characters",
-              },
-            })}
+            element="passwordRegister"
+            placeholder="confirmPassword"
+            className="p-2 block w-full rounded-md border shadow-sm outline-none"
+            validations={[
+              requiredValidator(),
+              minValidator(8),
+              maxValidator(26),
+            ]}
+            onInputHandler={onInputHandler}
+            sameValue={sameValue}
           />
-          {errors.password && (
-            <p className="text-red-700 text-xs mt-1">
-              {errors.password.message}
-            </p>
-          )}
         </div>
         <button
           type="submit"
+          onClick={sendUserData}
+          disabled={!sameValue && !formState.isFormValid}
           className="w-full mt-8 py-2 px-4 bg-blue-600 hover:bg-blue-700 duration-300 text-white-100 rounded-lg focus:outline-none"
         >
           Register
@@ -148,7 +169,7 @@ export default function Register() {
 
         <div className="mt-6 text-center">
           <Link className="text-xs" to="/shop/login">
-            Dont Have an Account? <span>Log In</span>
+            Don't Have an Account? <span>Log In</span>
           </Link>
         </div>
       </form>
