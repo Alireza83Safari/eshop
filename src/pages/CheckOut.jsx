f46ceb1989efcc6b5a67025cc66645737eed1171
Alireza <alireza83safarii@gmail.com>
2023-08-04 15:13:08 +0330

    CheckOut is completed

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -1,75 +1,147 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { faCartShopping, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
-import productsContext from "../Context/productsContext";
+
 import Breadcrumb from "../components/Breadcrumb";
+import Header from "./Header/Header";
+import Footer from "./Footer";
+import productsContext from "../Context/productsContext";
+import Spinner from "../components/Spinner/Spinner";
 
-export default function CheckOut() {
-  const { checkOut, setCheckOut } = useContext(productsContext);
-  const [quantities, setQuantities] = useState(
-    checkOut.reduce((acc, product) => {
-      acc[product.id] = 1;
-      return acc;
-    }, {})
-  );
-
-  const totalAmount = checkOut.reduce(
-    (acc, product) => acc + product.price * quantities[product.id],
+export default function Orders() {
+  const { token, isLoading } = useContext(productsContext);
+  const [orders, setOrders] = useState([]);
+  const totalAmount = orders.reduce(
+    (acc, order) => acc + order.price * order.quantity,
     0
   );
-
+  const totalTax = totalAmount / 10;
+  const totalDiscount = totalAmount / 20;
+  const totalPayment = Math.floor(totalAmount - totalDiscount - totalTax);
   const handleIncrement = (productId) => {
-    setQuantities((prevQuantities) => ({
-      ...prevQuantities,
-      [productId]: prevQuantities[productId] + 1,
-    }));
+    changeIncrementQuantity(productId.productItemId, productId.id);
   };
 
   const handleDecrement = (productId) => {
-    if (quantities[productId] > 1) {
-      setQuantities((prevQuantities) => ({
-        ...prevQuantities,
-        [productId]: prevQuantities[productId] - 1,
-      }));
-    } else if (quantities[productId] === 1) {
-      // If the quantity is 1, remove the product from the checkout
-      const updatedCheckOut = checkOut.filter(
-        (product) => product.id !== productId
-      );
-      setCheckOut(updatedCheckOut);
+    changeDecrementQuantity(productId.productItemId, productId.id);
+  };
+
+  const removeProductHandler = async (id) => {
+    try {
+      const response = await fetch(`/api/v1/orderItem/delete/${id}`, {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          Authorization: `${token}`,
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Failed to delete the product.");
+      }
+      console.log("Product successfully deleted.");
+      // update order
+      getOrder();
+    } catch (error) {
+      console.error("Error deleting the product:", error.message);
     }
   };
 
-  const removeProductHandler = (id) => {
-    let filterCheckOut = checkOut.filter((product) => product.id !== id);
-    setCheckOut(filterCheckOut);
+  // get orders from api
+  const getOrder = () => {
+    fetch("/api/v1/order", {
+      headers: {
+        accept: "application/json",
+        Authorization: `${token}`,
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((result) => {
+        setOrders(result.items);
+      });
+  };
+  const changeIncrementQuantity = async (itemId, id) => {
+    try {
+      let newQuantity = orders.find((order) => order.id === id).quantity + 1;
+
+      // Make the POST request to update the quantity
+      const response = await fetch("/api/v1/orderItem", {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `${token}`,
+        },
+        body: JSON.stringify({ productItemId: itemId, quantity: newQuantity }),
+      });
+
+      // Check if the request was successful
+      if (!response.ok) {
+        throw new Error("Failed to update the quantity.");
+      }
+
+      // Update the local state with the new quantity
+      getOrder();
+    } catch (error) {
+      console.log("Error updating quantity:", error.message);
+    }
   };
 
+  const changeDecrementQuantity = async (itemId, id) => {
+    try {
+      let newQuantity = orders.find((order) => order.id === id).quantity - 1;
+
+      // Make the POST request to update the quantity
+      const response = await fetch("/api/v1/orderItem", {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `${token}`,
+        },
+        body: JSON.stringify({ productItemId: itemId, quantity: newQuantity }),
+      });
+
+      // Check if the request was successful
+      if (!response.ok) {
+        throw new Error("Failed to update the quantity.");
+      }
+
+      // Update the local state with the new quantity
+      getOrder();
+    } catch (error) {
+      console.log("Error updating quantity:", error.message);
+    }
+  };
+
+  useEffect(() => {
+    getOrder();
+  }, []);
   return (
-    <div className="bg-white-300 dark:bg-black-200 text-black-900 my-32 dark:text-white-100">
-      <div className="flex justify-center mb-5">
-        <Breadcrumb
-          links={[
-            { id: 1, title: "Home", to: "shop/products" },
-            {
-              id: 2,
-              title: "Check out",
-              to: "shop/checkout",
-            },
-          ]}
-        />
-      </div>
-      <div className="flex justify-center">
-        <div>
-          <div className="relative">
+    <>
+      <Header />
+
+      <section className="bg-white-100  dark:bg-black-200 text-black-900 mt-4 mb-8 z-10 dark:text-white-100">
+        <div className="flex justify-center mb-5">
+          <Breadcrumb
+            links={[
+              { id: 1, title: "Home", to: "products" },
+              { id: 2, title: "Check out", to: "orders" },
+            ]}
+          />
+        </div>
+
+        <div className="flex justify-center">
+          <div className="relative max-h-[34rem] overflow-x-auto">
             <div className="flex items-center px-4 py-2 border-t border-x text-black-900 dark:text-white-100">
               <FontAwesomeIcon icon={faCartShopping} className="text-xl" />
               <p className="ml-2 text-lg">Shopping Cart</p>
             </div>
 
-            <div className="border px-5 lg:w-[40rem] w-screen">
-              {!checkOut.length ? (
+            <div className="border md:px-5 lg:w-[40rem] w-[90vw]">
+              {!orders.length ? (
                 <>
                   <div className="flex justify-center items-center">
                     <div className="w-80 h-80">
@@ -86,48 +158,42 @@ export default function CheckOut() {
                 </>
               ) : (
                 <>
-                  {Array.from(
-                    new Set(checkOut.map((product) => product.id))
-                  ).map((productId) => {
-                    const product = checkOut.find((p) => p.id === productId);
-                    return (
+                  {isLoading ? (
+                    <Spinner />
+                  ) : (
+                    orders.map((order) => (
                       <div
-                        key={product.id}
+                        key={order.id}
                         className="flex items-center border-b h-36"
                       >
                         <img
-                          src={product.img}
-                          alt={product.name}
-                          className="w-24 mx-4"
+                          src={`http://127.0.0.1:6060/${order.fileUrl}`}
+                          className="w-24 h-24 object-contain md:mx-4"
                         />
-                        <div className="ml-4">
-                          <p className="pb-2 text-sm">{product.name}</p>
-                          <p className="py-1 text-sm text-gray-800">
-                            ${product.price}
-                          </p>
-                          <p className="py-1 text-sm text-gray-800">
-                            Color: White
-                          </p>
+                        <div className="md:ml-4 md:text-sm text-xs">
+                          <p className="pb-2">{order.productName}</p>
+                          <p className="py-1 text-gray-800">${order.price}</p>
+                          <p className="py-1m text-gray-800">Color: White</p>
                         </div>
 
-                        <div className="absolute flex items-center right-10">
-                          <p className="text-sm">
-                            ${product.price * quantities[product.id]}
-                          </p>
+                        <div className="absolute flex items-center right-10 md:text-sm text-xs">
+                          <p>${order.price * order.quantity}</p>
 
-                          <div className="flex items-center mx-10">
+                          <div className="flex items-center md:mx-10 mx-4">
                             <button
-                              onClick={() => handleDecrement(product.id)}
-                              className="px-4 py-2 rounded-lg bg-gray-100 hover:bg-gray-200 duration-200 focus:outline-none"
+                              onClick={() => {
+                                handleDecrement(order);
+                              }}
+                              className="md:px-4 px-3 py-2 rounded-lg bg-gray-100 hover:bg-gray-200 duration-200 focus:outline-none"
                             >
                               -
                             </button>
-                            <span className="px-5">
-                              {quantities[product.id]}
+                            <span className="md:px-5 px-2">
+                              {order.quantity}
                             </span>
                             <button
-                              onClick={() => handleIncrement(product.id)}
-                              className="px-4 py-2 rounded-lg bg-gray-100 hover:bg-gray-200 duration-200 focus:outline-none"
+                              onClick={() => handleIncrement(order)}
+                              className="md:px-4 px-3 py-2 rounded-lg bg-gray-100 hover:bg-gray-200 duration-200 focus:outline-none"
                             >
                               +
                             </button>
@@ -136,68 +202,81 @@ export default function CheckOut() {
                             icon={faTrashAlt}
                             className="text-red-700"
                             onClick={() => {
-                              removeProductHandler(product.id);
+                              {
+                                removeProductHandler(order.productItemId);
+                              }
                             }}
                           />
                         </div>
                       </div>
-                    );
-                  })}
+                    ))
+                  )}
                 </>
               )}
             </div>
           </div>
-        </div>
-        <div className="lg:w-[20rem] lg:block hidden h-[20rem] px-6 py-2 ml-8 border text-sm">
-          <p className="py-2 text-xl font-bold text-center">Cart Total</p>
 
-          <p className="py-2 text-gray-800">{checkOut.length} Items</p>
-          <div className="flex justify-between border-b pt-2 pb-4">
-            <p>Subtotal</p>
-            <p>$94.00</p>
+          <div className="lg:w-[20rem] lg:block hidden h-[24rem] px-6 py-2 ml-8 border text-sm z-10 rounded-lg">
+            <p className="py-2 text-xl font-bold text-center">Cart Total</p>
+
+            <div className="flex justify-between pt-6 pb-2">
+              <p>Items</p>
+              <p className="text-blue-600 font-black">{orders.length}</p>
+            </div>
+
+            <div className="flex justify-between pt-6 pb-2">
+              <p>Total(Tax Excl.)</p>
+              <p>{totalAmount.toLocaleString()}$</p>
+            </div>
+
+            <div className="flex justify-between pt-6 pb-2">
+              <p>Discount Amount</p>
+              <p className="text-green-300">{totalDiscount}(20%)</p>
+            </div>
+
+            <div className="flex justify-between py-4 text-red-700">
+              <p>Taxes</p>
+              <p>{totalTax}$</p>
+            </div>
+
+            <div className="flex justify-between py-4 font-black">
+              <p>Total Payment</p>
+              <p>{totalPayment}$</p>
+            </div>
+
+            <button className=" w-full mt-3 py-2 bg-blue-600 text-xs text-white-100 rounded-lg">
+              <Link>Proceed to orders</Link>
+            </button>
           </div>
-          <div className="flex justify-between pt-4 pb-2">
-            <p className="">Total(Tax Excl.)</p>
-            <p className="">{totalAmount.toLocaleString()}$</p>
+          <div className="lg:hidden block fixed bottom-16 bg-white-100 w-full px-12">
+            <div className="flex text-sm justify-between py-2">
+              <p>Products Discount :</p>
+              <p className="text-green-300 font-black">{totalDiscount}$</p>
+            </div>
+            <div className="flex text-sm justify-between py-2">
+              <p>Total Tax :</p>
+              <p className="text-red-700 font-black">{totalTax}$</p>
+            </div>
+
+            <div className="flex text-sm justify-between py-2">
+              <p>Total Amount :</p>
+              <p className="font-black">{totalAmount}$</p>
+            </div>
           </div>
 
-          <div className="flex justify-between pt-4 pb-2">
-            <p className="">Discount Amount</p>
-            <p className="text-green-300">{totalAmount / 20}$ (20%)</p>
+          <div className="lg:hidden flex fixed bottom-0 w-full border-t py-3 bg-white-100">
+            <button className="w-1/3 text-white-100 rounded-lg py-2 mx-4 bg-blue-600">
+              Buy
+            </button>
+            <div className="absolute flex items-center right-0 text-lg">
+              <p className="mr-1-">Total Payment:</p>
+              <span className="mr-5">{totalPayment}$</span>
+            </div>
           </div>
+        </div>
+      </section>
 
-          <div className="flex justify-between py-2 text-red-700">
-            <p>Taxes</p>
-            <p>{totalAmount / 10}$</p>
-          </div>
-          <button className=" w-full mt-3 py-2 bg-blue-600 text-xs text-white-100 rounded-lg">
-            <Link>Proceed to checkout</Link>
-          </button>
-        </div>
-        <div className="lg:hidden block fixed bottom-20 bg-white-100 w-full px-12 pt-2">
-          <div className="flex text-sm justify-between py-2">
-            <p>product price:({checkOut.length})</p>
-            <p>{totalAmount}</p>
-          </div>
-          <div className="flex text-sm justify-between py-2">
-            <p>Total Price</p>
-            <p>{totalAmount}</p>
-          </div>
-          <div className="flex text-sm justify-between py-2 text-red-700">
-            <p>Your profit from the purchase:</p>
-            <p>{totalAmount / 10}</p>
-          </div>
-        </div>
-        <div className="lg:hidden flex fixed bottom-0 w-full border-t py-3 bg-white-100">
-          <button className=" w-1/2 text-white-100 rounded-lg py-2 mx-4 bg-blue-600">
-            Buy
-          </button>
-          <div className="absolute flex items-center right-0 text-lg">
-            <p className="mr-10">Total price:</p>
-            <span className="mr-5">5454$</span>
-          </div>
-        </div>
-      </div>
-    </div>
+      <Footer />
+    </>
   );
 }
