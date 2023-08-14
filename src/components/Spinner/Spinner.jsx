55a30235d788c77ef8d07465df5dea65ec973b2a
Alireza <alireza83safarii@gmail.com>
2023-08-14 10:26:05 +0330

    change width and height

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Spinner.css";
 export default function Spinner() {
   return (
-    <div className="flex justify-center items-center mt-56">
+    <div className="flex justify-center items-center">
       <div class="custom-loader"></div>
     </div>
   );
