7e025554cfaf2d71a8dbfa8030c1b7c8392e3f4a
Alireza <alireza83safarii@gmail.com>
2023-09-10 16:46:38 +0330

    change some routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,18 +3,18 @@ import { Navigate } from "react-router-dom";
 import Index from "../components/Admin";
 import Comments from "../pages/Admin/Comments";
 import Dashboard from "../pages/Admin/Dashboard";
-import ShopProducts from "../pages/Products";
-import ProductsInfo from "../pages/ProductsInfo";
+import Product from "../pages/ProductPage";
+import ProductInfo from "../pages/ProductInfoPage";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import PanelProducts from "../pages/Admin/Products";
-import Shipping from "../pages/Shipping";
+import Shipping from "../pages/ShippingPage";
 import SearchResult from "../pages/SearchResult";
 import Roles from "../pages/Admin/Roles";
 import Users from "../pages/Admin/Users";
 import Orders from "../pages/Admin/Orders";
 import CheckOut from "../pages/CheckOut";
-import ProfilePage from "../pages/ProfilePage";
+import Profile from "../pages/Profile";
 import ProfileAddress from "../components/Profile/ProfileAddress";
 import ProfileFavorite from "../components/Profile/ProfileFavorite";
 import ProfileOrders from "../components/Profile/ProfileOrders";
@@ -25,11 +25,12 @@ import BrandResult from "../pages/BrandResult";
 import CategoryResult from "../pages/CategoryResult";
 import Home from "../pages/Home";
 import AppPic from "../pages/Admin/AppPic";
+import ProfileEdit from "../components/Profile/ProfileEdit";
 
 const route = [
   { path: "", element: <Home /> },
-  { path: "products", element: <ShopProducts /> },
-  { path: "products/:productID", element: <ProductsInfo /> },
+  { path: "product", element: <Product /> },
+  { path: "product/:productID", element: <ProductInfo /> },
   { path: "checkout", element: <CheckOut /> },
   { path: "login", element: <Login /> },
   { path: "register", element: <Register /> },
@@ -39,12 +40,13 @@ const route = [
   { path: "/category/:category", element: <CategoryResult /> },
   {
     path: "profile/*",
-    element: <ProfilePage />,
+    element: <Profile />,
     children: [
       { path: "address", element: <ProfileAddress /> },
       { path: "favorite", element: <ProfileFavorite /> },
       { path: "orders", element: <ProfileOrders /> },
       { path: "comments", element: <ProfileComments /> },
+      { path: "edit", element: <ProfileEdit /> },
     ],
   },
   { path: "panel/login", element: <AdminLogin /> },
