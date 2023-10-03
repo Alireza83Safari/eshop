d142b2470f1e28db588d839a9d1e84bda5fa80e4
Alireza <alireza83safarii@gmail.com>
2023-10-03 10:47:49 +0330

    refactor

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,6 +13,7 @@ export default function Login() {
   const { userLogin } = useContext(AuthContext);
   const [errors, setErrors] = useState(null);
   const [isLoading, setLoading] = useState(false);
+  const [isDisable, setIsDisable] = useState(false);
   const [serverErrors, setServerErrors] = useState(null);
   const [loginInfos, setLoginInfos] = useState({
     username: "",
@@ -20,12 +21,17 @@ export default function Login() {
   });
 
   const loginInfosHandler = (event) => {
+    const { name, value } = event.target;
+
+    const isAnyInputEmpty = Object.values(loginInfos).some((val) => val === "");
+
+    setIsDisable(isAnyInputEmpty);
+
     setLoginInfos({
       ...loginInfos,
-      [event.target.name]: event.target.value,
+      [name]: value,
     });
   };
-
   const userLoginHandler = (event) => {
     event.preventDefault();
 
@@ -34,7 +40,6 @@ export default function Login() {
     userAxios
       .post("/login", loginInfos)
       .then((res) => {
-        console.log(res);
         if (res.status === 200) {
           userLogin();
           navigate("/");
@@ -50,80 +55,99 @@ export default function Login() {
     <>
       <Header />
       <Sidebar />
-      <section className="flex items-center justify-center mt-32 mb-10">
-        <form className="w-96 p-6 rounded-xl shadow-md bg-white-300 dark:bg-black-900">
-          <div className={` ${isLoading && "opacity-30"}`}>
-            <h2 className="text-2xl font-bold mb-6 text-center dark:text-white-200">
-              Login
-            </h2>
-            <span className="text-red-700 text-center text-xs">
-              {serverErrors?.message}
-            </span>
 
-            <div className="mb-4 mt-6">
-              <label
-                htmlFor="username"
-                className="block text-sm font-medium text-gray-700 dark:text-white-200"
-              >
-                username
-              </label>
-              <input
-                type="text"
-                name="username"
-                element="input"
-                placeholder="username"
-                className="p-2 block w-full rounded-md border shadow-sm outline-none"
-                onChange={loginInfosHandler}
-                value={loginInfos?.username}
-                onFocus={() => {
-                  setErrors("");
-                  setServerErrors("");
-                }}
-              />
-              <span className=" text-red-700 text-center text-xs">
-                {errors?.username} {serverErrors?.errors?.username}
-              </span>
-            </div>
-            <div className="mb-4 mt-6">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700 dark:text-white-200"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                placeholder="password"
-                className="p-2 block w-full rounded-md border shadow-sm outline-none"
-                onChange={loginInfosHandler}
-                value={loginInfos?.password}
-                onFocus={() => {
-                  setErrors("");
-                  setServerErrors("");
-                }}
-              />
-              <span className=" text-red-700 text-center text-xs">
-                {errors?.password} {serverErrors?.errors?.password}
-              </span>
-            </div>
+      <section className="grid lg:grid-cols-2 py-16 relative m-auto min-h-screen xl:px-20 px-5 z-10">
+        <div className="flex lg:justify-end justify-center items-center w-96 m-auto lg:py-0 py-10">
+          <div className="w-96">
+            <img src="/images/User-Account-Sign-up.png" />
           </div>
-          <button
-            type="submit"
-            className={`w-full mt-8 py-2 px-4 bg-blue-600 hover:bg-blue-700 duration-300 text-white-100 rounded-lg disabled:bg-gray-200 ${
-              isLoading && "py-5 bg-gray-200"
-            }`}
-            disabled={errors?.length}
-            onClick={userLoginHandler}
-          >
-            {isLoading ? <Spinner /> : "login"}
-          </button>
-          <div className="mt-6 text-center dark:text-white-200">
-            <Link className="text-xs" to="/register">
-              Dont Havent an Account? <span>Sign Up</span>
-            </Link>
-          </div>
-        </form>
+        </div>
+        <div className="flex lg:justify-start justify-center items-center">
+          <form className="sm:w-[26rem] w-full py-8 px-5 rounded-xl shadow-md bg-white-300 dark:bg-black-900">
+            <div className={` ${isLoading && "opacity-30"}`}>
+              <h2 className="text-2xl font-bold mb-8 text-center dark:text-white-200">
+                Login
+              </h2>
+              <span className="text-red-700 flex justify-center text-center text-xs">
+                {serverErrors?.message}
+              </span>
+
+              <div className="mb-4 mt-6">
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-medium text-gray-700 dark:text-white-200"
+                >
+                  username
+                </label>
+                <input
+                  type="text"
+                  name="username"
+                  element="input"
+                  placeholder="username"
+                  className="p-2 block w-full rounded-md border shadow-sm outline-none dark:bg-black-200"
+                  onChange={loginInfosHandler}
+                  value={loginInfos?.username}
+                  onFocus={() => {
+                    setErrors("");
+                    setServerErrors("");
+                  }}
+                />
+                <span className=" text-red-700 text-center text-xs">
+                  {errors?.username} {serverErrors?.errors?.username}
+                </span>
+              </div>
+              <div className="mb-4 mt-6">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700 dark:text-white-200"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  className="p-2 block w-full rounded-md border shadow-sm outline-none dark:bg-black-200"
+                  onChange={loginInfosHandler}
+                  value={loginInfos?.password}
+                  onFocus={() => {
+                    setErrors("");
+                    setServerErrors("");
+                  }}
+                />
+                <span className=" text-red-700 text-center text-xs">
+                  {errors?.password} {serverErrors?.errors?.password}
+                </span>
+              </div>
+            </div>
+            <button
+              type="submit"
+              className={`w-full mt-8 py-2 px-4 bg-blue-600 hover:bg-blue-700 duration-300 text-white-100 rounded-lg disabled:bg-gray-200 ${
+                isLoading && "py-5 bg-gray-200"
+              }`}
+              disabled={isDisable}
+              onClick={userLoginHandler}
+            >
+              {isLoading ? <Spinner /> : "login"}
+            </button>
+            <div className="mt-6 text-center dark:text-white-200">
+              <Link className="text-xs" to="/register">
+                Dont Havent an Account? <span>Sign Up</span>
+              </Link>
+            </div>
+          </form>
+        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+          className=" absolute bottom-0 -z-10"
+        >
+          <path
+            fill="#a2d9ff"
+            fillOpacity="0.1"
+            d="M0 128l60-16c60-16 180-48 300-21.3C480 117 600 203 720 218.7c120 16.3 240-37.7 360-48 120-10.7 240 21.3 300 37.3l60 16v96H0z"
+          ></path>
+        </svg>
       </section>
 
       <Footer />
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -13,6 +13,7 @@ export default function Register() {
   const [serverErrors, setServerErrors] = useState(null);
   const [sameValue, setSameValue] = useState(false);
   const [isLoading, setLoading] = useState(false);
+  const [isDisable, setIsDisable] = useState(false);
   const [errors, setErrors] = useState({
     username: "",
     password: "",
@@ -25,9 +26,17 @@ export default function Register() {
   });
 
   const registerInfosHandler = (event) => {
+    const { name, value } = event.target;
+
+    const isAnyInputEmpty = Object.values(registerInfos).some(
+      (val) => val === ""
+    );
+
+    setIsDisable(isAnyInputEmpty);
+
     setRegisterInfos({
       ...registerInfos,
-      [event.target.name]: event.target.value,
+      [name]: value,
     });
   };
 
@@ -37,6 +46,7 @@ export default function Register() {
 
   const sendUserData = async () => {
     registerValidation(registerInfos, errors, setErrors);
+
     if (registerInfos.password !== registerInfos.passwordConfirmation) {
       setSameValue(false);
       return;
@@ -62,109 +72,128 @@ export default function Register() {
     <>
       <Header />
       <Sidebar />
-      <div className="flex items-center justify-center my-12 mt-32 mb-10">
-        <form
-          className="w-96 p-6 rounded-xl shadow-md bg-white-300 dark:bg-black-900"
-          onSubmit={(e) => e.preventDefault()}
-        >
-          <div className={` ${isLoading && "opacity-30"}`}>
-            <h2 className="text-2xl font-bold mb-6 text-center dark:text-white-200">
-              Register
-            </h2>
-
-            <div className="mb-4 mt-6">
-              <label
-                htmlFor="username"
-                className="block text-sm font-medium text-gray-700 dark:text-white-200"
-              >
-                Username
-              </label>
-              <input
-                type="text"
-                name="username"
-                id="username"
-                placeholder="Username"
-                className="p-2 block w-full rounded-md border shadow-sm outline-none"
-                onChange={registerInfosHandler}
-                value={registerInfos?.username}
-                onFocus={() => {
-                  setErrors("");
-                  setServerErrors("");
-                }}
-              />
-              <span className="text-red-700 text-center text-xs">
-                {errors?.username} {serverErrors?.username}
-              </span>
-            </div>
-
-            <div className="mb-4 mt-6">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700 dark:text-white-200"
-              >
-                Password
-              </label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                placeholder="password"
-                className="p-2 block w-full rounded-md border shadow-sm outline-none"
-                onChange={registerInfosHandler}
-                sameValue={sameValue}
-                value={registerInfos?.password}
-                onFocus={() => {
-                  setErrors("");
-                  setServerErrors("");
-                }}
-              />
-              <span className=" text-red-700 text-center text-xs">
-                {errors?.password} {serverErrors?.password}
-              </span>
-            </div>
-            <div className="mb-4 mt-6">
-              <label
-                htmlFor="passwordConfirmation"
-                className="block text-sm font-medium text-gray-700 dark:text-white-200"
-              >
-                Confirm Password
-              </label>
-              <input
-                type="password"
-                id="passwordConfirmation"
-                name="passwordConfirmation"
-                placeholder="passwordConfirmation"
-                className="p-2 block w-full rounded-md border shadow-sm outline-none"
-                onChange={registerInfosHandler}
-                value={registerInfos?.passwordConfirmation}
-                sameValue={sameValue}
-                onFocus={() => {
-                  setErrors("");
-                  setServerErrors("");
-                }}
-              />
-              <span className=" text-red-700 text-center text-xs">
-                {errors?.passwordConfirmation} {serverErrors?.password}
-              </span>
-            </div>
+      <section className="grid lg:grid-cols-2 py-16 relative m-auto min-h-screen xl:px-20 px-5 z-10">
+        <div className="flex lg:justify-end justify-center items-center w-96 m-auto lg:py-0 py-10">
+          <div className="w-96">
+            <img src="/images/User-Account-Sign-up.png" />
           </div>
-          <button
-            type="submit"
-            onClick={sendUserData}
-            className={`w-full mt-8 py-2 px-4 bg-blue-600 hover:bg-blue-700 duration-300 text-white-100 rounded-lg disabled:bg-gray-200  ${
-              isLoading && "py-5 bg-gray-200"
-            }`}
+        </div>
+        <div className="flex lg:justify-start justify-center items-center">
+          <form
+            className="w-96 p-6 rounded-xl shadow-md bg-white-300 dark:bg-black-900 z-20"
+            onSubmit={(e) => e.preventDefault()}
           >
-            {isLoading ? <Spinner /> : "Register"}
-          </button>
+            <div className={` ${isLoading && "opacity-30"}`}>
+              <h2 className="text-2xl font-bold mb-6 text-center dark:text-white-200">
+                Register
+              </h2>
 
-          <div className="mt-6 text-center dark:text-white-200">
-            <Link className="text-xs" to="/login">
-              Don't Have an Account? <span>Log In</span>
-            </Link>
-          </div>
-        </form>
-      </div>
+              <div className="mb-4 mt-6">
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-medium text-gray-700 dark:text-white-200"
+                >
+                  Username
+                </label>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  placeholder="Username"
+                  className="p-2 block w-full rounded-md border shadow-sm outline-none"
+                  onChange={registerInfosHandler}
+                  value={registerInfos?.username}
+                  onFocus={() => {
+                    setErrors("");
+                    setServerErrors("");
+                  }}
+                />
+                <span className="text-red-700 text-center text-xs">
+                  {errors?.username} {serverErrors?.username}
+                </span>
+              </div>
+
+              <div className="mb-4 mt-6">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-700 dark:text-white-200"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="password"
+                  className="p-2 block w-full rounded-md border shadow-sm outline-none"
+                  onChange={registerInfosHandler}
+                  sameValue={sameValue}
+                  value={registerInfos?.password}
+                  onFocus={() => {
+                    setErrors("");
+                    setServerErrors("");
+                  }}
+                />
+                <span className=" text-red-700 text-center text-xs">
+                  {errors?.password} {serverErrors?.password}
+                </span>
+              </div>
+              <div className="mb-4 mt-6">
+                <label
+                  htmlFor="passwordConfirmation"
+                  className="block text-sm font-medium text-gray-700 dark:text-white-200"
+                >
+                  Confirm Password
+                </label>
+                <input
+                  type="password"
+                  id="passwordConfirmation"
+                  name="passwordConfirmation"
+                  placeholder="password"
+                  className="p-2 block w-full rounded-md border shadow-sm outline-none"
+                  onChange={registerInfosHandler}
+                  sameValue={sameValue}
+                  value={registerInfos?.passwordConfirmation}
+                  onFocus={() => {
+                    setErrors("");
+                    setServerErrors("");
+                  }}
+                />
+                <span className=" text-red-700 text-center text-xs">
+                  {errors?.passwordConfirmation} {serverErrors?.password}
+                </span>
+              </div>
+            </div>
+            <button
+              type="submit"
+              onClick={sendUserData}
+              className={`w-full mt-8 py-2 px-4 bg-blue-600 hover:bg-blue-700 duration-300 text-white-100 rounded-lg disabled:bg-gray-200 z-10 ${
+                isLoading && "py-5 bg-gray-200"
+              }`}
+              disabled={isDisable || !sameValue}
+            >
+              {isLoading ? <Spinner /> : "Register"}
+            </button>
+
+            <div className="mt-6 text-center dark:text-white-200">
+              <Link className="text-xs" to="/login">
+                Don't Have an Account? <span>Log In</span>
+              </Link>
+            </div>
+          </form>
+        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+          className=" absolute bottom-0 -z-10"
+        >
+          <path
+            fill="#a2d9ff"
+            fillOpacity="0.1"
+            d="M0 128l60-16c60-16 180-48 300-21.3C480 117 600 203 720 218.7c120 16.3 240-37.7 360-48 120-10.7 240 21.3 300 37.3l60 16v96H0z"
+          ></path>
+        </svg>
+      </section>
       <Footer />
     </>
   );
