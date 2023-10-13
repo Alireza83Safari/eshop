4ea280e2b4fde1f6ffccfb6ee1324ccb5d5c47f3
Alireza <alireza83safarii@gmail.com>
2023-10-13 14:59:47 +0330

    get zero scroll when change location

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import { useRoutes } from "react-router-dom";
+import { useLocation, useRoutes } from "react-router-dom";
 import route from "./routes/routes";
 import "./App.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ThemeContextProvider } from "./Context/ThemeContext";
 import { AuthContextProvider } from "./Context/AuthContext";
+import { useEffect } from "react";
 
 function App() {
   const routes = useRoutes(route);
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
   return (
     <div className="App max-w-[1400px] sm:mx-auto relative w-full min-w-full">
       <ThemeContextProvider>
